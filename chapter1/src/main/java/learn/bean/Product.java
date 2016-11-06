c0b16419f7ccc3a5132133c161e75d6f30f8572c
petea <smart__petea@mail.ru>
2016-11-06 09:07:04 +0200

    passing arguments to a constructor

--- chapter1/src/main/java/learn/bean/Product.java
+++ chapter1/src/main/java/learn/bean/Product.java
@@ -1,10 +1,23 @@
 package learn.bean;
 
-public class Product {
+import java.io.Serializable;
+
+public class Product implements Serializable {
+    private static final long serialVersionUID = 748392348L;
     private String name;
+    private String description;
+    private float price;
 
     public Product() {
-        System.out.println("create the product " + this);
+        System.out.println("non featured product " + this);
+    }
+
+    public Product(String name, String description, float price) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+
+        System.out.println("featured product " + this);
     }
 
     public void setName(String name) {
@@ -13,5 +26,21 @@ public class Product {
 
     public String getName() {
         return this.name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
     }
 }
