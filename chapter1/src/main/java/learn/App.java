cfe9974881939d92a401517375ab4c21b612e142
petea <smart__petea@mail.ru>
2016-11-06 08:42:24 +0200

    ch1 - xml configuration

---
+++ chapter1/src/main/java/learn/App.java
@@ -0,0 +1,12 @@
+package learn;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        ApplicationContext appContext = new ClassPathXmlApplicationContext("config.xml");
+    }
+}
